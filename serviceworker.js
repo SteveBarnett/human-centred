717ed0c1be9316090ec144c8b353d658cf819790
Steve Barnett <steve@naga.co.za>
2018-04-28 13:34:03 +0200

    Refactor service worker a bit

--- serviceworker.js
+++ serviceworker.js
@@ -1,18 +1,51 @@
 ---
 ---
-const version = '0.0.5';
+const version = '0.0.2';
 const staticCacheName = version + 'staticfiles';
-const imageCacheName = 'images';
+const imagesCacheName = 'images';
 const pagesCacheName = 'pages';
+
 const cacheList = [
   staticCacheName,
-  imageCacheName,
+  imagesCacheName,
   pagesCacheName
 ];
 
-function stashInCache(cacheName, request, response) {
-  caches.open(cacheName)
-      .then( cache => cache.put(request, response) );
+const offlinePages = [
+  '/',
+  '/about/',
+  '/community/',
+  '/offline/'
+];
+
+function updateStaticCache() {
+  return caches.open(staticCacheName)
+      .then( cache => {
+          // These items won't block the installation of the Service Worker
+          cache.addAll([
+            '/img/logo.jpg',
+            '/wp-content/uploads/2016/09/ctfeds-400x282.jpg',
+            '/wp-content/uploads/2016/09/devicelab-400x225.jpg',
+            '/img/SteveBarnett.jpg',
+            '/wp-content/uploads/2016/09/workshop-opening-400x225.jpg',
+            '/wp-content/uploads/2016/09/IMG_1422-400x266.jpg'
+          ]);
+          // These items must be cached for the Service Worker to complete installation
+          return cache.addAll([
+            '/css/style.css?ver={{ site.css-version }}',
+            '/img/offline.svg'
+          ].concat(offlinePages));
+      });
+}
+
+function clearOldCaches() {
+  return caches.keys()
+      .then( keys => {
+          return Promise.all(keys
+              .filter(key => !cacheList.includes(key))
+              .map(key => caches.delete(key))
+          );
+      });
 }
 
 function trimCache(cacheName, maxItems) {
@@ -28,112 +61,79 @@ function trimCache(cacheName, maxItems) {
   });
 }
 
-addEventListener('install', installEvent => {
-  skipWaiting();
-  installEvent.waitUntil(
-    caches.open(staticCacheName).then(staticCache => {
-      staticCache.addAll([
-        '/img/logo.jpg'
-      ]);
-      return staticCache.addAll([
-        '/css/style.css?ver={{ site.css-version }}',
-        '/',
-        '/about/',
-        '/community/',
-        '/offline/',
-        '/img/offline.svg'
-      ]);
-    })
+function stashInCache(cacheName, request, response) {
+  caches.open(cacheName)
+      .then( cache => cache.put(request, response) );
+}
+
+self.addEventListener('install', event => {
+  event.waitUntil(updateStaticCache()
+      .then( () => self.skipWaiting() )
   );
 });
 
-self.addEventListener('activate', activateEvent => {
-  activateEvent.waitUntil(
-    caches.keys()
-    .then(cacheNames => {
-      return Promise.all(
-        cacheNames.map( cacheName => {
-          if(!cacheName.includes(staticCacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-    .then( () => {
-      return clients.claim();
-    })
+self.addEventListener('activate', event => {
+  event.waitUntil(clearOldCaches()
+      .then( () => self.clients.claim() )
   );
 });
 
-addEventListener('message', messageEvent => {
-  if(messageEvent.data === 'clean up caches') {
+self.addEventListener('message', event => {
+  if(event.data === 'clean up caches') {
     trimCache(pagesCacheName, 10);
-    trimCache(imageCacheName, 20);
+    trimCache(imagesCacheName, 20);
   }
 })
 
-addEventListener('fetch', fetchEvent => {
-  const request = fetchEvent.request;
+self.addEventListener('fetch', event => {
+  let request = event.request;
+  let url = new URL(request.url);
 
+  if (request.method !== 'GET') {
+      return;
+  }
+
+  // For HTML requests, try the network first, fall back to the cache, finally the offline page
   if (request.headers.get('Accept').includes('text/html')) {
-    fetchEvent.respondWith(
+    event.respondWith(
       fetch(request)
-      .then( responseFromFetch => {
-        let copy = responseFromFetch.clone();
-        fetchEvent.waitUntil(
-          stashInCache(pagesCacheName, request, copy)
-        );
-        return responseFromFetch;
-      })
-      .catch(error => {
-        return caches.match(request)
-        .then( responseFromCache => {
-          if (responseFromCache) {
-            return responseFromCache;
+        .then( response => {
+          // Stash a copy of this page in the pages cache
+          let copy = response.clone();
+          if (offlinePages.includes(url.pathname) || offlinePages.includes(url.pathname + '/')) {
+            stashInCache(staticCacheName, request, copy);
+          } else {
+            stashInCache(pagesCacheName, request, copy);
           }
-          return caches.match('/offline/');
+          return response;
         })
-      })
+        .catch( () => {
+          return caches.match(request)
+            .then( response => response || caches.match('/offline/') );
+        })
     );
     return;
   }
 
-  if (request.headers.get('Accept').includes(' image')) {
-    fetchEvent.respondWith(
-      caches.match(request)
-      .then( responseFromCache => {
-        if (responseFromCache) {
-          fetchEvent.waitUntil(
-            fetch(request)
-            .then (responseFromFetch => {
-              stashInCache(imageCacheName, request, responseFromFetch)
-            })
-          )
-          return responseFromCache;
-        }
-        return fetch(request)
-        .then( responseFromFetch => {
-          let copy = responseFromFetch.clone();
-          fetchEvent.waitUntil(
-            stashInCache(imageCacheName, request, copy)
-          );
-          return responseFromFetch;
+  // For non-HTML requests, look in the cache first, fall back to the network
+  event.respondWith(
+    caches.match(request)
+      .then(response => {
+        return response || fetch(request)
+        .then( response => {
+          // If the request is for an image, stash a copy of this image in the images cache
+          if (request.headers.get('Accept').includes('image')) {
+            let copy = response.clone();
+            stashInCache(imagesCacheName, request, copy);
+          }
+          return response;
         })
-        .catch( error => {
-          return caches.match('/img/fallback.svg');
+        .catch( () => {
+          // If the request is for an image, show an offline placeholder
+          if (request.headers.get('Accept').includes('image')) {
+            caches.match('img/fallback.svg');
+          }
         });
       })
-    );
-    return;
-  }
-
-  fetchEvent.respondWith(
-    caches.match(request)
-    .then( responseFromCache => {
-      if (responseFromCache) {
-        return responseFromCache;
-      }
-      return fetch(request);
-    })
   );
 });
