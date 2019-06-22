cc57dcee616d82d956cdfa0c6ff6f56f81074bd3
Steve Barnett <steve@naga.co.za>
2019-06-22 16:23:57 +0200

    Bump SW version

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
----
----
-const version = '0.0.12';
+-- -
+-- -
+const version = '0.0.13';
 const staticCacheName = 'staticfiles-' + version;
 const imagesCacheName = 'images';
 const pagesCacheName = 'pages';
